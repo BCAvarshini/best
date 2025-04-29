45c7168951eaabf0d43477ea9436c9d02303f521
BCAvarshini <varshinijakkali@gmail.com>
2025-04-29 11:19:27 +0530

    saved

---
+++ Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int id = 12345;
+        String course = "Computer Science";
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Course: " + course);
+    }
+}
